bc733316569a9f4f0361a69adb65fc29b82b7f14
Andrey Vinichenko <andrey.vinichenko@gmail.com>
2018-02-15 16:14:25 +0100

    Initial commit

---
+++ src/AmeotokoIsotopeEventsBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ameotoko\IsotopeEventsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Andrey Vinichenko
+ */
+class AmeotokoIsotopeEventsBundle extends Bundle
+{
+}
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ameotoko\IsotopeEvents\ContaoManager;
+
+use Ameotoko\IsotopeEvents\AmeotokoIsotopeEventsBundle;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(AmeotokoIsotopeEventsBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class, 'isotope'])
+        ];
+    }
+}
