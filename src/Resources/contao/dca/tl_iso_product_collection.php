36a26439fce6f1d04c0574224118c319af86f028
Andrey Vinichenko <andrey.vinichenko@gmail.com>
2018-03-21 18:13:24 +0100

    Orders module: implemented filter by event name in Backend

--- src/Resources/contao/dca/tl_iso_product_collection.php
+++ src/Resources/contao/dca/tl_iso_product_collection.php
@@ -2,9 +2,21 @@
 
 $GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['label']['label_callback'] = ['IsotopeEvents\Backend\ProductCollection\CustomCallback', 'getOrderLabel'];
 
+// We need to use on of existing fields and replace it with our contents, so we don't alter the table
 $GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['label']['fields'][] = 'config_id';
 
 if ('BE' == TL_MODE && \Input::get('do') == 'iso_orders') {
 	$GLOBALS['TL_DCA']['tl_iso_product_collection']['fields']['billing_address_id']['label'] = 'Name';
 	$GLOBALS['TL_DCA']['tl_iso_product_collection']['fields']['config_id']['label'] = 'Events';
 }
+
+// Events filter
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['sorting']['panelLayout'] .= ';events_filter';
+
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['sorting']['panel_callback']['events_filter'] = ['IsotopeEvents\Backend\ProductCollection\CustomCallback', 'generateEventsFilter'];
+
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['config']['onload_callback'][] = ['IsotopeEvents\Backend\ProductCollection\CustomCallback', 'applyEventsFilter'];
+
+// Don't need this filter
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['fields']['shipping_id']['filter'] = false;
+
--- src/Resources/contao/library/IsotopeEvents/Backend/ProductCollection/Callback.php
+++ src/Resources/contao/library/IsotopeEvents/Backend/ProductCollection/Callback.php
@@ -3,8 +3,10 @@
 namespace IsotopeEvents\Backend\ProductCollection;
 
 
+use Contao\Model\Collection;
 use Isotope\Backend\ProductCollection\Callback;
 use Isotope\Isotope;
+use Isotope\Model\Product;
 use Isotope\Model\ProductCollection\Order;
 
 class CustomCallback extends Callback
@@ -58,4 +60,84 @@ class CustomCallback extends Callback
 
 		return $args;
 	}
+
+	public function generateEventsFilter()
+	{
+		/** @var Collection $orders */
+		$orders = Order::findBy(
+			['type = ?', 'order_status != ?', 'locked != ?'],
+			['order', '0', '']
+		);
+
+		$arrOptions = [];
+
+		/** @var Order $objOrder */
+		foreach ($orders as $objOrder) {
+
+			foreach ($objOrder->getItems() as $objOrderItem) {
+
+				$objProduct = $objOrderItem->getProduct();
+
+				// Only unique products in array
+				$arrOptions[$objProduct->getProductId()] = [
+					'id' => $objProduct->getProductId(),
+					'name' => $objProduct->getName()
+				];
+			}
+		}
+
+		$session = \Session::getInstance()->getData();
+
+		$strHtml = '<div class="tl_subpanel" style="width: 60%">
+		<strong>Filter events:</strong>
+		<select name="eventsFilter" id="eventsFilter" class="tl_select" style="width: 70%; margin-left: 3px;">
+		  <option value="eventsFilter">Event</option>
+		  <option value="eventsFilter">---</option>';
+
+		foreach ($arrOptions as $option) {
+			$strHtml .= sprintf('<option value="%s"%s>%s</option>',
+				$option['id'],
+				($session['filter']['eventsFilter'] == $option['id']) ? ' selected' : '',
+				$option['name']);
+		}
+
+		$strHtml .= '</select></div>';
+
+		return $strHtml;
+	}
+
+	public function applyEventsFilter()
+	{
+		$session = \Session::getInstance()->getData();
+
+		if (\Input::post('FORM_SUBMIT') == 'tl_filters') {
+
+			$session['filter']['eventsFilter'] = \Input::post('eventsFilter');
+			\Session::getInstance()->setData($session);
+		}
+
+		if (\Input::post('filter_reset')) {
+
+			$session['filter']['eventsFilter'] = 'eventsFilter';
+			\Session::getInstance()->setData($session);
+		}
+
+		if ($session['filter']['eventsFilter'] != 'eventsFilter') {
+
+			/** @var Product $objProduct */
+			if ($objProduct = Product::findByPk($session['filter']['eventsFilter'])) {
+				$arrVariants = $objProduct->getVariantIds ();
+
+				$arrVariants[] = $objProduct->getId ();
+
+				$res = \Database::getInstance ()
+					->query ('SELECT id FROM tl_iso_product_collection WHERE id IN (SELECT pid FROM tl_iso_product_collection_item WHERE product_id IN (' . implode (',', $arrVariants) . '))');
+
+				$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['sorting']['filter'][] =
+					['id IN (' . implode (',', $res->fetchEach ('id')) . ')', ''];
+			}
+		}
+
+	}
+
 }
