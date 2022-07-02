0c85e8b7a446a7472c01b9d8d6d0288cbd134c2b
ameotoko <andrey.vinichenko@gmail.com>
2022-07-02 17:48:34 +0200

    convert old language files to XLIFF

--- src/Resources/contao/languages/en/modules.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['IMD']['location'] = ['Locations', 'Set up event locations'];
--- src/Resources/contao/languages/en/tl_module.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['tl_module']['calendar_legend'] = 'Calendar settings';
-$GLOBALS['TL_LANG']['tl_module']['asCalendar'] = ['Display as calendar', 'Use a calendar layout in frontend.'];
